60b67164c41e7b42243eab334c429b3ef2dd019b
Kimmix <kimmix05@gmail.com>
2023-06-15 23:02:19 +0700

    add font, sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
+    webfontDownload(),
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
