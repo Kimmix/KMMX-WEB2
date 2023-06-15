0658d55110f7cc61b32a53f5a198850234c8a64d
Kimmix <kimmix05@gmail.com>
2023-06-15 20:07:06 +0700

    deploy to deno

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from 'svelte-adapter-deno';
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
 }
